1987df56a8fddc957bc00d56f00604e588d0b623
ravi0424 <102217635+ravi0424@users.noreply.github.com>
2022-10-26 21:09:14 +0530

    #include <bits/stdc++.h> using namespace std; int main() {     float a;     float b;     cin>>a;     b=(a-32.0)*5.0/9.0;     cout<<fixed<<setprecision(2)<<b;
        return 0;
    }

---
+++ Fahrenheit_to_Celsius.c
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float a;
+    float b;
+    cin>>a;
+    b=(a-32.0)*5.0/9.0;
+    cout<<fixed<<setprecision(2)<<b;
+    
+    return 0;
+}
