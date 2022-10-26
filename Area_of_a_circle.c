2ec78d7db952a539acb1d51a93df15ab917da4c9
ravi0424 <102217635+ravi0424@users.noreply.github.com>
2022-10-26 20:56:11 +0530

    #include <bits/stdc++.h> using namespace std; int main() {     int r;     float b;     cin>>r;     b=3.14*(r*r);     cout << fixed << setprecision(2) << b;     return 0; }

---
+++ Area_of_a_circle.c
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int r;
+    float b;
+    cin>>r;
+    b=3.14*(r*r);
+    cout << fixed << setprecision(2) << b; 
+    return 0;
+}
