ab7a5591d57bcd49e0d26cbdc95c2b3244c891d2
ravi0424 <102217635+ravi0424@users.noreply.github.com>
2022-10-26 23:44:24 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int a;     cin>>a;     float b=pow(a,0.5);     int c=b;     if (b==c)     {         cout<<"True";     }     else     {         cout<<"False";     } }

---
+++ perfect_square_root_or_not.c
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    float b=pow(a,0.5);
+    int c=b;
+    if (b==c)
+    {
+        cout<<"True";
+    }
+    else
+    {
+        cout<<"False";
+    }
+}
