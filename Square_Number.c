6546df3a07daabb5aeec7c65dee1618f5e7002af
ravi0424 <102217635+ravi0424@users.noreply.github.com>
2022-10-28 00:51:33 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int a;     cin>>a;     float b=pow(a,0.5);     int c=b;     if (c==b){         cout<<"True";     }     else{         cout<<"False";     } }

---
+++ Square_Number.c
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    float b=pow(a,0.5);
+    int c=b;
+    if (c==b){
+        cout<<"True";
+    }
+    else{
+        cout<<"False";
+    }
+}
